f8f918b33f34132f7718844ab6fd546ed26a56e0
a.23308051280301 <a.23308051280301@cbtis128.edu.mx>
2026-02-11 18:15:17 +0000

    Act 7 Paola Ortega

--- lib/main.dart
+++ lib/main.dart
@@ -1,122 +1,112 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(AppEventos());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class AppEventos extends StatelessWidget {
+  const AppEventos({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      home: Clientes(),
     );
   }
-}
+}//fin AppEventos
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class Clientes extends StatelessWidget {
+  const Clientes({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
+    return Scaffold(    
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+  title: const Text(
+    '"Eventos Woody" Paola Ortega 6J',
+    style: TextStyle(color: Colors.black),
+  ),
+  centerTitle: true,
+  backgroundColor: const Color.fromARGB(255, 107, 218, 255), // Azul clarito (LightBlue)
+  leading: const Icon(
+    Icons.cake, // Icono de pastel
+    color: Color.fromARGB(255, 10, 75, 129),
+  ),
+  actions: const [
+    Icon(
+      Icons.castle, // Icono de globo
+      color: Color.fromARGB(255, 10, 75, 129),
+    ),
+    SizedBox(width: 15),
+    Icon(
+      Icons.calendar_month, // Icono de calendario
+      color: Colors.black,
+    ),
+    SizedBox(width: 15),
+  ],
+),
+body: ListView(
+          padding: const EdgeInsets.all(16.0),
+          children: const [
+            TarjetaUsuario(
+              nombre: 'Jesusa Campoya',
+              profesion: 'Diseñadora',
+              idImagen: 'https://raw.githubusercontent.com/paola-ortega-0301/Im-genes-para-Flutter-6J-11-02-26/refs/heads/main/1.png',
+            ),
+            TarjetaUsuario(
+              nombre: 'Lizet Silva',
+              profesion: 'Admin. Empresas',
+              idImagen: 'https://raw.githubusercontent.com/paola-ortega-0301/Im-genes-para-Flutter-6J-11-02-26/refs/heads/main/2.png',
+            ),
+            TarjetaUsuario(
+              nombre: 'Lucía Fuentes de Ortiz',
+              profesion: 'Homeless',
+              idImagen: 'https://raw.githubusercontent.com/paola-ortega-0301/Im-genes-para-Flutter-6J-11-02-26/refs/heads/main/3.png',
             ),
           ],
         ),
+    );
+  }
+}//fin clase clientes
+
+class TarjetaUsuario extends StatelessWidget {
+  final String nombre;
+  final String profesion;
+  final String idImagen;
+
+  const TarjetaUsuario({
+    super.key,
+    required this.nombre,
+    required this.profesion,
+    required this.idImagen,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: const Color.fromARGB(255, 5, 48, 114), // Fondo morado atractivo
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+      elevation: 5,
+      margin: const EdgeInsets.only(bottom: 16),
+      child: ListTile(
+        contentPadding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        leading: CircleAvatar(
+          backgroundImage: NetworkImage(idImagen),
+        ),
+        title: Text(
+          nombre,
+          style: const TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
+        subtitle: Text(
+          profesion,
+          style: const TextStyle(color: Colors.white70),
+        ),
+        trailing: IconButton(
+          icon: const Icon(Icons.favorite_border, color: Color.fromARGB(255, 113, 186, 228)),
+          onPressed: () {
+            // Aquí iría la lógica del "Like"
+          },
+        ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
